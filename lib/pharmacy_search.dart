246c144dc1e0da4b6c3623286fb8eb6d8d75cfe1
GoGyoonGa <ouna2@naver.com>
2023-05-30 20:32:35 +0900

    2023-05-30 20:32

--- lib/pharmacy_search.dart
+++ lib/pharmacy_search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class PharmacySearch extends StatefulWidget {
   const PharmacySearch({Key? key}) : super(key: key);
@@ -13,33 +14,65 @@ class _PharmacySearchState extends State<PharmacySearch> {
 
   final LatLng _center = const LatLng(37.333043, 127.944499);
 
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
+  void _addMarker(LatLng position) {
+    final markerId = MarkerId(position.toString());
+    final marker = Marker(
+      markerId: markerId,
+      position: position,
+      infoWindow: InfoWindow(title: 'Pharmacy'),
+    );
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+  void _getCurrentLocation() async {
+    var locationPermission = await Geolocator.checkPermission();
+
+    if (locationPermission == LocationPermission.denied) {
+      // 위치 권한이 거부되었을 경우 처리
+      return;
+    }
+
+    var position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      _addMarker(LatLng(position.latitude, position.longitude));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Container(width: MediaQuery.of(context).size.width,
-    height: MediaQuery.of(context).size.height,);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Pharmacy Map'),
-          backgroundColor: Colors.green[700],
+          backgroundColor: Colors.green,
         ),
         body: GoogleMap(
-          
-        mapType: MapType.normal, //지도 유형 설정
+          mapType: MapType.normal,
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 20.0,
           ),
+          markers: _markers,
         ),
-        
       ),
     );
   }
 }
-
-
